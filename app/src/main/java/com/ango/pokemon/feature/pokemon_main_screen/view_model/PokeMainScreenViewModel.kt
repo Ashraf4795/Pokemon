2a633204efc4d6d3e93ae5b7e9ed0c09dcf792b9
Ashraf4795 <ashraf.mohamediti4728@gmail.com>
2021-04-16 03:48:19 +0200

    add getPokemonDetails function to get pokemon details list after receiving pokemon object

--- app/src/main/java/com/ango/pokemon/feature/pokemon_main_screen/view_model/PokeMainScreenViewModel.kt
+++ app/src/main/java/com/ango/pokemon/feature/pokemon_main_screen/view_model/PokeMainScreenViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ango.pokemon.core.data.model.Pokemon
+import com.ango.pokemon.core.data.model.PokemonDetails
 import com.ango.pokemon.core.repository.Repository
+import com.ango.pokemon.core.utils.getPokemonId
 import com.ango.pokemon.core.utils.status_wrapper.State
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -19,6 +21,9 @@ class PokeMainScreenViewModel(
     private val _pokemon = MutableLiveData<State<Pokemon>>()
     val pokemon: LiveData<State<Pokemon>> = _pokemon
 
+    private val _pokemonDetails = MutableLiveData<MutableList<PokemonDetails>>()
+    val pokemonDetails: LiveData<MutableList<PokemonDetails>> = _pokemonDetails
+
 
     fun getPokemon() {
         viewModelScope.launch(IO) {
@@ -28,4 +33,15 @@ class PokeMainScreenViewModel(
         }
     }
 
+    fun getPokemonDetails(pokemon: Pokemon) {
+        viewModelScope.launch(IO) {
+            val pokemonDetailsCollection = mutableListOf<PokemonDetails>()
+            pokemon.results.forEach { result ->
+                val pokemonId = getPokemonId(result.url)
+                val pokemonDetail = repository.getPokemonDetails(pokemonId)
+                pokemonDetailsCollection.add(pokemonDetail)
+            }
+            _pokemonDetails.postValue(pokemonDetailsCollection)
+        }
+    }
 }
